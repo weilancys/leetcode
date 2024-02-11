2c4524518a3229fd49286a2b57d1ab48797a8d20
lbcoder <lbcoder@hotmail.com>
2024-02-11 18:48:46 +0800

    20 valid parentheses solution 1 done.

---
+++ 20_valid_parentheses/20_valid_parentheses.md
@@ -0,0 +1,11 @@
+### question
+- https://leetcode.com/problems/valid-parentheses/
+
+### rank
+- easy
+
+### solution
+- -
+
+### notes
+- -
---
+++ 20_valid_parentheses/solution1.py
@@ -0,0 +1,46 @@
+s1 = "({[]})"
+s2 = "()[]{}"
+s3 = "()[[]]"
+s4 = "([)}"
+
+# the main idea is to use a stack and put each parenthesis on the stack
+# if the current parenthesis closes the previous one, pop the stack, otherwise put the current parenthesis on the stack
+# fully closed parentheses should have a clean stack in the end
+
+class Solution:
+    def isValid(self, s: str) -> bool:
+        stack = []
+        for letter in s:
+            if letter == "(" or letter == "{" or letter == "[":
+                stack.append(letter)
+            elif letter == ")":
+                try:
+                    if stack[-1] == "(":
+                        stack.pop()
+                    else:
+                        stack.append(letter)
+                except IndexError:
+                    return False
+            elif letter == "}":
+                try:
+                    if stack[-1] == "{":
+                        stack.pop()
+                    else:
+                        stack.append(letter)
+                except IndexError:
+                    return False
+            elif letter == "]":
+                try:
+                    if stack[-1] == "[":
+                        stack.pop()
+                    else:
+                        stack.append(letter)
+                except IndexError:
+                    return False
+        return len(stack) == 0
+
+
+if __name__ == "__main__":
+    solution1 = Solution()
+    result = solution1.isValid(s2)
+    print(result)
