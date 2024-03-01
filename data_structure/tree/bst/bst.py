568c1e651ec28617841be12115cf6e08eb266cea
lbcoder <lbcoder@hotmail.com>
2024-03-01 18:01:34 +0800

    rewrote insert method, updated search method and added postorder and preorder traversal.

--- data_structure/tree/bst/bst.py
+++ data_structure/tree/bst/bst.py
@@ -1,4 +1,6 @@
-# reference: https://www.javatpoint.com/binary-search-tree
+# references:
+# https://www.javatpoint.com/binary-search-tree
+# https://www.geeksforgeeks.org/binary-search-tree-traversal-inorder-preorder-post-order/
 
 class BST:
     class Node:
@@ -10,42 +12,48 @@ class BST:
     def __init__(self):
         self.__root = None
 
-    def add(self, val):
-        def _add(walk, val):
-            if val < walk.val:
-                if walk.left:
-                    _add(walk.left, val)
-                else:
-                    new_node = self.Node(val)
-                    walk.left = new_node
-            elif val > walk.val:
-                if walk.right:
-                    _add(walk.right, val)
-                else:
-                    new_node = self.Node(val)
-                    walk.right = new_node
-            else:
-                raise ValueError("bst should not contain identical values.")
-        
-        if self.__root:
-            _add(self.__root, val)
+    def __insert(self, walk, val):
+        if walk is None:
+            node = self.Node(val)
+            return node
+        elif val < walk.val:
+            walk.left = self.__insert(walk.left, val)
+            return walk
+        elif val > walk.val:
+            walk.right = self.__insert(walk.right, val)
+            return walk
         else:
-            new_node = self.Node(val)
-            self.__root = new_node
-            
-    def search(self, val):
-        def _search(walk, val):
-            if not walk:
-                return
-            elif walk.val == val:
+            return walk
+
+    def insert(self, val):
+        self.__root = self.__insert(self.__root, val)
+
+    def __search_recursive(self, walk, val):
+        # search for a node in bst in recursive manner
+        if not walk:
+            return
+        elif walk.val == val:
+            return walk
+        elif val < walk.val:
+            return self.__search(walk.left, val)
+        elif val > walk.val:
+            return self.__search(walk.right, val)
+    
+    def __search_loop(self, walk, val):
+        # search for a node in bst in loop manner
+        while walk:
+            if walk.val == val:
                 return walk
             elif val < walk.val:
-                return _search(walk.left, val)
+                walk = walk.left
             elif val > walk.val:
-                return _search(walk.right, val)
-        return _search(self.__root, val)
+                walk = walk.right
+        return None
+            
+    def search(self, val):
+        return self.__search_loop(self.__root, val)
         
-    def traversal(self):
+    def inorder_traversal(self):
         arr = []
         def _traversal(node, list):
             if not node:
@@ -55,12 +63,41 @@ class BST:
             _traversal(node.right, arr)
         _traversal(self.__root, arr)
         return arr
+    
+    def preorder_traversal(self):
+        arr = []
+        def _traversal(node, list):
+            if not node:
+                return
+            list.append(node.val)
+            _traversal(node.left, arr)
+            _traversal(node.right, arr)
+        _traversal(self.__root, arr)
+        return arr
+ 
+    def postorder_traversal(self):
+        arr = []
+        def _traversal(node, list):
+            if not node:
+                return
+            _traversal(node.left, arr)
+            _traversal(node.right, arr)
+            list.append(node.val)
+        _traversal(self.__root, arr)
+        return arr
 
 
 if __name__ == "__main__":
-    l = [45, 15, 79, 90, 10, 55, 12, 20, 50]
+    # l = [45, 15, 79, 90, 10, 55, 12, 20, 50]
+    # l = [45, 45, 45, 15, 79, 90, 10, 55, 12, 20, 50]
+    l = [100, 20, 200, 10, 30, 150, 300]
     bst = BST()
     for i in l:
-        root = bst.add(i)
-    arr = bst.traversal()
-    print(arr)
+        bst.insert(i)
+    # arr = bst.inorder_traversal()
+    # node = bst.search(45)
+    # print(node.val)
+    
+    print(bst.inorder_traversal())
+    print(bst.preorder_traversal())
+    print(bst.postorder_traversal())
